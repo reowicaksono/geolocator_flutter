87d8e6b267a834dd5474d8bf8f2a8532dde9cdce
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-02-14 16:46:37 +0700

    [update] set up page auto route

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:presence_app/modules/routes/app_routes.dart';
+import 'package:presence_app/router.dart';
+
+final _appRoute = AppRouter();
+
+class PrecenseApp extends StatelessWidget {
+  const PrecenseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _appRoute.config(),
+    );
+  }
+}
