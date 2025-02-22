7c68fc4443ab34530a1841e9a3dba7b8a7d1e0c7
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-02-22 20:54:48 +0700

    [update] initilize workmanager for background process

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:presence_app/app.dart';
+import 'package:workmanager/workmanager.dart';
+
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    return Future.value(true);
+  });
+}
 
 void main() {
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+
   runApp(const PrecenseApp());
 }
